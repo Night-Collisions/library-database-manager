13464c413eff0bb67c5a2edc6ca5b55dda21c3af
vixorem <priorityvctr@gmail.com>
2019-01-06 12:01:20 +1000

    add new table UDC

---
+++ Client/src/libapp/model/UDC.java
@@ -0,0 +1,17 @@
+package libapp.model;
+
+public class UDC {
+    private String code;
+
+    public UDC(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ Client/src/libapp/view/UDCController.java
@@ -0,0 +1,78 @@
+package libapp.view;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import libapp.model.UDC;
+
+import static javafx.scene.input.MouseEvent.MOUSE_CLICKED;
+
+public class UDCController {
+    private Main main;
+    private ObservableList<UDC> udcs =
+            FXCollections.observableArrayList();
+
+    @FXML
+    private TableView<UDC> table;
+    @FXML
+    private TableColumn<UDC, String> code;
+
+    @FXML
+    private void initialize() {
+        fillTable();
+        setEvents();
+
+        code.setCellValueFactory(new PropertyValueFactory<>("code"));
+
+        table.setItems(udcs);
+    }
+
+    private void fillTable() {
+        // TODO: ебашим запрос к серверу и заполняем
+
+    }
+
+    private void setEvents() {
+        ContextMenu context = new ContextMenu();
+        MenuItem insert = new MenuItem("Добавить");
+        MenuItem edit = new MenuItem("Редактировать");
+        MenuItem delete = new MenuItem("Удалить");
+
+        context.getItems().add(insert);
+        context.getItems().add(edit);
+        context.getItems().add(delete);
+
+        //TODO: нахуячить, если это библиотекарь или че то такое
+        insert.setOnAction(t -> {
+            //TODO: нахуячить
+        });
+
+        //TODO: нахуячить, если это библиотекарь или че то такое
+        edit.setOnAction(t -> {
+            //TODO: нахуячить
+        });
+
+        //TODO: нахуячить, если это библиотекарь или че то такое
+        delete.setOnAction(t -> {
+            //TODO: нахуячить
+        });
+
+        table.addEventHandler(MOUSE_CLICKED, t -> {
+            if(t.getButton() == MouseButton.SECONDARY) {
+                context.show(table, t.getScreenX(), t.getScreenY());
+            } else {
+                context.hide();
+            }
+        });
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
