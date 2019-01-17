4439c386d07a66b16a4bffbeb82529c86efd6481
vixorem <priorityvctr@gmail.com>
2019-01-18 05:20:39 +1000

    add editor edit

--- Client/src/libapp/view/Editor/EditorChangeController.java
+++ Client/src/libapp/view/Editor/EditorChangeController.java
@@ -1,6 +1,12 @@
 package libapp.view.Editor;
 
+import libapp.ClientSocket;
 import libapp.view.Main;
+import libapp.view.MessageController;
+
+import java.time.format.DateTimeFormatter;
+
+import static libapp.QueryParser.buildQuery;
 
 public class EditorChangeController extends EditorWinController{
     protected String ID;
@@ -8,6 +14,8 @@ public class EditorChangeController extends EditorWinController{
     public EditorChangeController(Main main, String id) {
         super(main);
         ID = id;
+        sexDict.put("женский", "1");
+        sexDict.put("мужской", "0");
     }
 
     protected void initialize() {
@@ -20,5 +28,33 @@ public class EditorChangeController extends EditorWinController{
 
     protected void applyChange() {
         super.applyChange();
+        try {
+            String result = "";
+            socket = ClientSocket.enableConnection(socket);
+
+            String[] args = {
+                    main.getUser().getId(),
+                    "changeEditor",
+                    ID,
+                    name.getText(),
+                    surname.getText(),
+                    patronymic.getText(),
+                    sexDict.get(sex.getValue()),
+                    bornDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    deathDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    phone.getText(),
+                    email.getText()
+            };
+
+            result = socket.makeRequest(buildQuery(args));
+
+            if (!result.equals("ok")) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            new MessageController("Ошибка",
+                    "Не удалось вставить запись", e);
+        }
     }
 }
