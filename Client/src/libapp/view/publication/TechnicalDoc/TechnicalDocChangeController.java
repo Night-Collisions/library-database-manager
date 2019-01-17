e7b03954ded9ca21a9125aeffe88b11653414da8
vixorem <priorityvctr@gmail.com>
2019-01-18 06:12:05 +1000

    update

--- Client/src/libapp/view/publication/TechnicalDoc/TechnicalDocChangeController.java
+++ Client/src/libapp/view/publication/TechnicalDoc/TechnicalDocChangeController.java
@@ -1,6 +1,12 @@
 package libapp.view.publication.TechnicalDoc;
 
+import libapp.ClientSocket;
 import libapp.view.Main;
+import libapp.view.MessageController;
+
+import java.time.format.DateTimeFormatter;
+
+import static libapp.QueryParser.buildQuery;
 
 public class TechnicalDocChangeController extends TechnicalDocWinController {
     protected String ID;
@@ -21,5 +27,27 @@ public class TechnicalDocChangeController extends TechnicalDocWinController {
 
     protected void applyChange() {
         super.applyChange();
+
+        try {
+            String result = "";
+            socket = ClientSocket.enableConnection(socket);
+            //TODO
+            String[] args = {
+                    main.getUser().getId(),
+                    "changeDocs",
+                    ID,
+                    name.getText(),
+                    organization.getValue().getId()};
+
+            result = socket.makeRequest(buildQuery(args));
+
+            if (!result.equals("ok")) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            new MessageController("Ошибка",
+                    "Не удалось вставить запись", e);
+        }
     }
 }
