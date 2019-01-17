451d7e54d40c8dcbb55d835dabdce2be2140586d
vixorem <priorityvctr@gmail.com>
2019-01-18 01:35:26 +1000

    add article insert

--- Client/src/libapp/view/publication/AllPublication/PublicationAddController.java
+++ Client/src/libapp/view/publication/AllPublication/PublicationAddController.java
@@ -39,7 +39,7 @@ public class PublicationAddController extends PropertyWin {
                 new Pair<>("publication" + File.separator + "Book" + File.separator + "BookAddOverview.fxml", new BookAddController(main)),
                 new Pair<>("publication" + File.separator + "Book" + File.separator + "BookAddOverview.fxml", new WorkAddController(main)),
                 new Pair<>("publication" + File.separator + "Article" + File.separator + "ArticleAddOverview.fxml", new ArticleAddController(main)),
-                new Pair<>("publication" + File.separator + "Thesis" + File.separator + "ThesisAddOverview.fxml", new ThesisAddController()),
+                new Pair<>("publication" + File.separator + "Thesis" + File.separator + "ThesisAddOverview.fxml", new ThesisAddController(main)),
                 new Pair<>("publication" + File.separator + "TechnicalDoc" + File.separator + "TechnicalDocAddOverview.fxml", new TechnicalDocAddController(main))
         );
     }
--- Client/src/libapp/view/publication/Thesis/ThesisAddController.java
+++ Client/src/libapp/view/publication/Thesis/ThesisAddController.java
@@ -1,12 +1,41 @@
 package libapp.view.publication.Thesis;
 
+import libapp.ClientSocket;
+import libapp.view.Main;
+import libapp.view.MessageController;
+
+import static libapp.QueryParser.buildQuery;
+
 public class ThesisAddController extends ThesisWinController {
     protected void initialize() {
         super.initialize();
-        name.setText("add");
+    }
+
+    public ThesisAddController(Main main) {
+        super(main);
     }
 
     protected void applyChange() {
         super.applyChange();
+
+        try {
+            String result = "";
+            socket = ClientSocket.enableConnection(socket);
+
+            String[] args = {
+                        main.getUser().getId(),
+                        "addDigestTheses",
+                        name.getText(),
+                        idDict.get(where.getValue())};
+
+            result = socket.makeRequest(buildQuery(args));
+            System.out.println(result);
+            if (!result.equals("ok")) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            new MessageController("Ошибка",
+                    "Не удалось вставить запись", e);
+        }
     }
 }
