89a458fba4b14f9fc808b983aba2fc317bacb5cd
vixorem <priorityvctr@gmail.com>
2019-01-18 03:55:36 +1000

    fix bug in insert organization

--- Client/src/libapp/view/Editor/EditorAddController.java
+++ Client/src/libapp/view/Editor/EditorAddController.java
@@ -1,11 +1,55 @@
 package libapp.view.Editor;
 
+import libapp.ClientSocket;
+import libapp.view.Main;
+import libapp.view.MessageController;
+
+import java.time.format.DateTimeFormatter;
+
+import static libapp.QueryParser.buildQuery;
+
 public class EditorAddController extends EditorWinController {
     protected void initialize() {
         super.initialize();
+        sexDict.put("мужской", "0");
+        sexDict.put("женский", "1");
+        sex.setEditable(false);
+        sex.getSelectionModel().selectFirst();
+    }
+
+    public EditorAddController(Main main) {
+        super(main);
     }
 
     protected void applyChange() {
         super.applyChange();
+
+        try {
+            String result = "";
+            socket = ClientSocket.enableConnection(socket);
+            String[] args = {
+                    main.getUser().getId(),
+                    "addEditor",
+                    name.getText(),
+                    surname.getText(),
+                    patronymic.getText(),
+                    sexDict.get(sex.getValue()),
+                    bornDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    deathDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                    phone.getText(),
+                    email.getText()
+            };
+
+            result = socket.makeRequest(buildQuery(args));
+            System.out.println(buildQuery(args));
+            System.out.println(result);
+            if (!result.equals("ok")) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            new MessageController("Ошибка",
+                    "Не удалось вставить запись", e);
+        }
     }
 }
--- Client/src/libapp/view/organization/OrganizationChangeController.java
+++ Client/src/libapp/view/organization/OrganizationChangeController.java
@@ -1,9 +1,14 @@
 package libapp.view.organization;
 
+import libapp.view.Main;
+
 public class OrganizationChangeController extends OrganizationWinController {
     protected void initialize() {
         super.initialize();
-        name.setText("ch");
+    }
+
+    public OrganizationChangeController(Main main) {
+        super(main);
     }
 
     protected void applyChange() {
